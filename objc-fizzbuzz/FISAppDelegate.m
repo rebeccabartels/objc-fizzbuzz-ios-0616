0ba3b26332493c9ed89c53069c5e3e8a1403f83e
rebeccabartels <rebeccakbartels@icloud.com>
2016-05-17 15:26:09 -0400

    Done.

--- objc-fizzbuzz/FISAppDelegate.m
+++ objc-fizzbuzz/FISAppDelegate.m
@@ -9,7 +9,80 @@
     
     /*
      
-     * Write your code here!
+     for (NSUInteger i = 1; i <= 100; i++) {
+     
+     }
+     for (NSUInteger i = 1; i <= 100; i++) {
+     NSLog(@"%lu", i);
+     }
+     for (NSUInteger i = 1; i <= 100; i++) {
+     NSString *result = @"";
+     result = [NSString stringWithFormat:@"%lu", i];
+     NSLog(@"%@", result);
+     }
+     or (NSUInteger i = 1; i <= 100; i++) {
+     NSString *result = @"";
+     
+     if (i % 3 == 0) {
+     result = @"Fizz";
+     }
+     else {
+     result = [NSString stringWithFormat:@"%lu", i];
+     }
+     NSLog(@"%@", result);
+     }
+     
+     for (NSUInteger i = 1; i <= 100; i++) {
+     NSString *result = @"";
+     
+     if (i % 3 == 0) {
+     result = @"Fizz";
+     }
+     else if (i % 5 == 0) {
+     result = @"Buzz";
+     }
+     else {
+     result = [NSString stringWithFormat:@"%lu", i];
+     }
+     NSLog(@"%@", result);
+     }
+     for (NSUInteger i = 1; i <= 100; i++) {
+     NSString *result = @"";
+     
+     if (i % 3 == 0 && i % 5 == 0) {
+     result = @"FizzBuzz";
+     }
+     else if (i % 3 == 0) {    // becomes an 'else if' statement
+     result = @"Fizz";
+     }
+     else if (i % 5 == 0) {
+     result = @"Buzz";
+     }
+     else {
+     result = [NSString stringWithFormat:@"%lu", i];
+     }
+     NSLog(@"%@", result);
+     }
+     NSUInteger start = 1;
+     NSUInteger limit = 100;
+     
+     for (NSUInteger i = start; i <= limit; i++) {
+     NSString *result = @"";
+     
+     if (i % 3 == 0 && i % 5 == 0) {
+     result = @"FizzBuzz";
+     }
+     else if (i % 3 == 0) {    // becomes an 'else if' statement
+     result = @"Fizz";
+     }
+     else if (i % 5 == 0) {
+     result = @"Buzz";
+     }
+     else {
+     result = [NSString stringWithFormat:@"%lu", i];
+     }
+     NSLog(@"%@", result);
+     }
      
      */
     
